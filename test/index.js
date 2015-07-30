7561785618ce66e83621f8611f5586b0e7cd78fd
Michael Williams <dinosaur@riseup.net>
2015-07-30 18:03:41 +1200

    fix tests

--- test/index.js
+++ test/index.js
@@ -3,11 +3,14 @@ var test = require('tape')
 var sync = require('../')
 
 test('sync this repo', function (t) {
-  sync({
+  var job = sync({
     remoteUrl: 'git://github.com/holodex/git-sync',
     localDir: __dirname + '/dir',
-    cronTime: '* 5 * * * *'
-  }, function (commit) {
-    console.log(commit)
+    cronTime: '*/5 * * * * *'
+  }, function (err, tree) {
+    t.error(err)
+    t.ok(tree.id())
+    job.stop()
+    t.end()
   })
 })
