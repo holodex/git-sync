69109c2d931cded14326fbf37082eefc7a1bc047
Michael Williams <dinosaur@riseup.net>
2015-08-19 09:57:58 +1200

    add failing test

--- test/index.js
+++ test/index.js
@@ -3,14 +3,25 @@ var test = require('tape')
 var sync = require('../')
 
 test('sync this repo', function (t) {
+  var lastSynced = null
   var job = sync({
     remoteUrl: 'git://github.com/holodex/git-sync',
     localDir: __dirname + '/dir',
     cronTime: '*/5 * * * * *'
-  }, function (err, tree) {
+  }, function (err, commit) {
     t.error(err)
-    t.ok(tree.id())
+
+    var commitId = commit.id()
+    if (lastSynced == null) {
+      t.ok(commitId, 'first sync')
+    } else {
+      t.notOk(!!commitId.equal(lastSynced), 'next sync is fresh commit')
+    }
+    lastSynced = commitId
+  })
+
+  setTimeout(function () {
     job.stop()
     t.end()
-  })
+  }, 15*1000)
 })
